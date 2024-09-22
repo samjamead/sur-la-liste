c25b7917e91fdd78446cf7211fdcea48ef74164b
Sam Mead <samjamead@gmail.com>
2024-09-22 16:19:34 +0200

    New todo page

--- app/login/sign-in.tsx
+++ app/login/sign-in.tsx
@@ -1,6 +1,6 @@
-import { SubmitButton } from "./submit-button";
-import { createClient } from "@/utils/supabase/server";
-import { redirect } from "next/navigation";
+import { SubmitButton } from './submit-button';
+import { createClient } from '@/utils/supabase/server';
+import { redirect } from 'next/navigation';
 
 export default function SignIn({
   searchParams,
@@ -8,10 +8,10 @@ export default function SignIn({
   searchParams: { message: string };
 }) {
   const signIn = async (formData: FormData) => {
-    "use server";
+    'use server';
 
-    const email = formData.get("email") as string;
-    const password = formData.get("password") as string;
+    const email = formData.get('email') as string;
+    const password = formData.get('password') as string;
     const supabase = createClient();
 
     const { error } = await supabase.auth.signInWithPassword({
@@ -26,39 +26,39 @@ export default function SignIn({
       );
     }
 
-    return redirect("/protected");
+    return redirect('/todo');
   };
 
   return (
-    <form className="flex w-full flex-col justify-center gap-2 rounded-md border p-8 text-foreground animate-in">
+    <form className='flex w-full flex-col justify-center gap-2 rounded-md border p-8 text-foreground animate-in'>
       {searchParams?.message && (
-        <p className="mb-4 rounded-md bg-foreground/10 p-4 text-center text-foreground">
+        <p className='mb-4 rounded-md bg-foreground/10 p-4 text-center text-foreground'>
           {searchParams.message}
         </p>
       )}
-      <label className="text-md" htmlFor="email">
+      <label className='text-md' htmlFor='email'>
         Email
       </label>
       <input
-        className="mb-6 rounded-md border bg-inherit px-4 py-2"
-        name="email"
-        placeholder="you@example.com"
+        className='mb-6 rounded-md border bg-inherit px-4 py-2'
+        name='email'
+        placeholder='you@example.com'
         required
       />
-      <label className="text-md" htmlFor="password">
+      <label className='text-md' htmlFor='password'>
         Password
       </label>
       <input
-        className="mb-6 rounded-md border bg-inherit px-4 py-2"
-        type="password"
-        name="password"
-        placeholder="••••••••"
+        className='mb-6 rounded-md border bg-inherit px-4 py-2'
+        type='password'
+        name='password'
+        placeholder='••••••••'
         required
       />
       <SubmitButton
         formAction={signIn}
-        className="mb-2 rounded-md bg-sky-900 px-4 py-2 text-foreground"
-        pendingText="Signing In..."
+        className='mb-2 rounded-md bg-sky-900 px-4 py-2 text-foreground'
+        pendingText='Signing In...'
       >
         Sign In
       </SubmitButton>
--- app/todo/page.tsx
+++ app/todo/page.tsx
@@ -14,12 +14,7 @@ export default async function ProtectedPage() {
 
   return (
     <div className='flex-1 w-full flex flex-col gap-20 items-center'>
-      <div className='w-full'>
-        <div className='py-6 font-bold bg-purple-950 text-center'>
-          This is a protected page that you can only see as an authenticated
-          user
-        </div>
-      </div>
+      <h2>Todo: </h2>
     </div>
   );
 }
